f0d39a257de1d4a97eea6cd883437fd5592fad1f
Vicko Pratama <vickokakuse21@gmail.com>
2021-11-08 19:13:08 +0700

    ci4

---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Us</h1>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque corporis tenetur asperiores, eveniet cumque quaerat odio aspernatur debitis deleniti doloribus laudantium, sunt voluptatem repudiandae natus. Est ipsam labore dolor eaque.</P>
+            <h5>Loh kok gitu wkwkwk</h5> 
+        </div>
+    </div>
+</div>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,8 @@
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Hello, world!</h1>
+                <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque corporis tenetur asperiores, eveniet cumque quaerat odio aspernatur debitis deleniti doloribus laudantium, sunt voluptatem repudiandae natus. Est ipsam labore dolor eaque.</P>
+            </div>
+        </div>
+    </div>
